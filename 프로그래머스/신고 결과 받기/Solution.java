9bf41e55668ba86591d752902edfff5d89512f9c
feedbaek <nicek789@naver.com>
2022-02-15 19:26:37 +0900

    숙제완료

--- 프로그래머스/신고 결과 받기/Solution.java
+++ 프로그래머스/신고 결과 받기/Solution.java
@@ -1,33 +1,29 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 class Solution {
 	public int[] solution(String[] id_list, String[] report, int k) {
 		int[] answer = new int[id_list.length];
-		Map<String, String> reportMap = new HashMap<>(); // 유저당 신고한 유저들 이름
+		Map<String, ArrayList<String>> reportMap = new HashMap<>(); // 유저당 신고한 유저들 이름
 		Map<String, Integer> mailMap = new HashMap<>();  // 유저들이 메일 받는 수
-		for (int i=0; i<id_list.length; i++)
+		for (int i=0; i<id_list.length; i++) {
 			mailMap.put(id_list[i], 0);	// 초기화
+			reportMap.put(id_list[i], new ArrayList<>());
+		}
 		for (int i=0; i<report.length; i++) {
 			String[] tmp = report[i].split(" ");
-			if (reportMap.containsKey(tmp[1])) {
-				if (reportMap.get(tmp[1]).contains(tmp[0]))	// 중복신고 패스
-					continue;
-				String str = reportMap.get(tmp[1]);
-				reportMap.put(tmp[1], str + " " + tmp[0]);
-			}
-			else
-				reportMap.put(tmp[1], tmp[0]);
+			if (reportMap.get(tmp[1]).contains(tmp[0]))	// 중복신고 패스
+				continue;
+			reportMap.get(tmp[1]).add(tmp[0]);
 		}
 		for (int i=0; i<id_list.length; i++) {
-			if (!reportMap.containsKey(id_list[i]))
-				continue;
-			String[] tmp = reportMap.get(id_list[i]).split(" ");
-			if (tmp.length < k)
+			ArrayList<String> tmp = reportMap.get(id_list[i]);
+			if (tmp.size() < k)
 				continue ;
-			for (int j=0; j< tmp.length; j++) {
-				int value = mailMap.get(tmp[j]);
-				mailMap.put(tmp[j], value + 1);
+			for (int j=0; j< tmp.size(); j++) {
+				int value = mailMap.get(tmp.get(j));
+				mailMap.put(tmp.get(j), value + 1);
 			}
 		}
 		for (int i=0; i<id_list.length; i++)
@@ -39,7 +35,7 @@ class Solution {
 		String[] id_list1 = {"muzi", "frodo", "apeach", "neo"};
 		String[] id_list2 = {"con", "ryan"};
 		String[] report1 = {"muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"};
-		String[] report2 = {"ryan con", "ryan con", "ryan con", "ryan con"};
+		String[] report2 = {"muzi ryan", "ryan con", "ryan con", "ryan con", "ryan con", "con ryan", "con ryan", "con ryan", "con ryan"};
 		for (int ans : s.solution(id_list1, report1, 2)) {
 			System.out.print(ans);
 		}
