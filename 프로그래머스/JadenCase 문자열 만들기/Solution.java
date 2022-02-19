4889daf17114789c77a90c4fd1ed26f67650fe82
feedbaek <nicek789@naver.com>
2022-02-20 00:56:28 +0900

    clean

--- 프로그래머스/JadenCase 문자열 만들기/Solution.java
+++
@@ -1,6 +0,0 @@
-class Solution {
-	public String solution(String s) {
-		String answer = "";
-		return answer;
-	}
-}
--- 프로그래머스/자릿수 더하기/Solution.java
+++
@@ -1,8 +0,0 @@
-public class Solution {
-	public int solution(int n) {
-		int answer = 0;
-		for (; n > 0; n /= 10)
-			answer += n % 10;
-		return answer;
-	}
-}
