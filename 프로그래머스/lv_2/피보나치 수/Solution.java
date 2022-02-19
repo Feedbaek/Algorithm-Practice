e840b27d1ced2f1cd409ce17ec6c59064ddc54a2
feedbaek <nicek789@naver.com>
2022-02-20 00:54:38 +0900

    day17

---
+++ 프로그래머스/lv_2/피보나치 수/Solution.java
@@ -0,0 +1,12 @@
+class Solution {
+	public int solution(int n) {
+		int[] arr = new int[n+1];
+		for (int i=0; i<=n; i++) {
+			if (i==0 || i==1)
+				arr[i] = i;
+			else
+				arr[i] = (arr[i-1] + arr[i-2]) % 1234567;
+		}
+		return arr[n];
+	}
+}
