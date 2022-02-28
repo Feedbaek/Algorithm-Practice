0d5f10e6532d572af16172a372e8d9eda58d5d36
feedbaek <nicek789@naver.com>
2022-03-01 00:32:27 +0900

    day23

---
+++ 백준/단계별로 풀기/정렬/Main.java
@@ -0,0 +1,10 @@
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+
+public class Main {
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(BufferedInputStream(in));
+		int n = Integer.parseInt(br.readLine());
+		//for (int )
+	}
+}
