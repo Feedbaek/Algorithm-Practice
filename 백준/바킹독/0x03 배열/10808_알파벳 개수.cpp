b960660030688340821b683c1789c6b1d59e2bd4
feedbaek <nicek789@naver.com>
2022-03-22 20:10:15 +0900

    바킹독 3강 배열

---
+++ 백준/바킹독/0x03 배열/10808_알파벳 개수.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   10808_알파벳 개수.cpp                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: minskim2 <minskim2@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/22 18:03:33 by minskim2          #+#    #+#             */
+/*   Updated: 2022/03/22 19:36:55 by minskim2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include<iostream>
+using namespace std;
+
+int	main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	string word;
+	int alp[26] = {};
+	cin >> word;
+	for (int i=0; i<word.size(); i++)
+		alp[word[i]-'a']++;
+	for (int i:alp)
+		cout << i << " ";
+	cout << '\n';
+	return (0);
+}
