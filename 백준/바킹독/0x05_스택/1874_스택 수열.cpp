daadb664ebc1f04f1a4fdf1db89196f1c18d159a
feedbaek <nicek789@naver.com>
2022-03-26 12:13:56 +0900

    바킹독 5강 문제풀기

--- 백준/바킹독/0x05_스택/1874_스택 수열.cpp
+++ 백준/바킹독/0x05_스택/1874_스택 수열.cpp
@@ -6,37 +6,36 @@
 /*   By: minskim2 <minskim2@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/26 08:45:46 by minskim2          #+#    #+#             */
-/*   Updated: 2022/03/26 11:43:36 by minskim2         ###   ########.fr       */
+/*   Updated: 2022/03/26 12:12:58 by minskim2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
-#include <vector>
 using namespace std;
 
 int	main(void) {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 	int n;
-	vector<string> v;
+	char v[200000];
+	int v_idx = 0;
 	int stack[100000];
 	int last_idx = -1;
 	cin >> n;
-	for (int i=0, x=0, flag=0; i<n; i++) {
-		int c;
+	for (int i=0, x=0, c; i<n; i++) {
 		cin >> c;
 		for (; x<c; x++) {
-			v.push_back("+");
+			v[v_idx++] ='+';
 			stack[++last_idx] = x+1;
 		}
 		if (stack[last_idx] != c) {
 			cout << "NO" << '\n';
 			return (0);
 		}
-		v.push_back("-");
+		v[v_idx++] = '-';
 		last_idx--;
 	}
-	for (string s:v)
-		cout << s << '\n';
+	for (int i=0; i<v_idx; i++)
+		cout << v[i] << '\n';
 	return (0);
 }
