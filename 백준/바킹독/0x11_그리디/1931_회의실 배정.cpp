f3a77484194fb6f8afb3061168062fc048457a60
feedbaek <nicek789@naver.com>
2022-07-06 13:23:15 +0900

    바킹독 그리디

--- 백준/바킹독/0x11_그리디/1931_회의실 배정.cpp
+++ 백준/바킹독/0x11_그리디/1931_회의실 배정.cpp
@@ -6,16 +6,35 @@
 /*   By: minskim2 <minskim2@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/01 12:14:02 by minskim2          #+#    #+#             */
-/*   Updated: 2022/07/01 16:05:45 by minskim2         ###   ########.fr       */
+/*   Updated: 2022/07/06 13:22:26 by minskim2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <utility>
+#include <algorithm>
 using namespace std;
+
+bool compare(pair<int, int>& a, pair<int, int>& b) {
+	if (a.second == b.second)
+		return a.first < b.first;
+	return a.second < b.second;
+}
 
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
-	int n;
+	pair<int, int>arr[100000];
+	int n, time=0, cnt=0;
 	cin >> n;
+	for (int i=0, x, y; i<n; i++)
+		cin >> arr[i].first >> arr[i].second;
+	sort(arr, arr+n, compare);
+	for (int i=0; i<n; i++) {
+		if (arr[i].first < time)
+			continue;
+		time = arr[i].second;
+		cnt++;
+	}
+	cout << cnt;
 }
