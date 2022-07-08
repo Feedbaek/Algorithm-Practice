310fec75d8af3215878255fc929a144b82f6614e
feedbaek <nicek789@naver.com>
2022-07-08 12:08:21 +0900

    바킹독 그리디

--- 백준/바킹독/0x11_그리디/2217_로프.cpp
+++ 백준/바킹독/0x11_그리디/2217_로프.cpp
@@ -1,40 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   1931_회의실 배정.cpp                                    :+:      :+:    :+:   */
+/*   2217_로프.cpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: minskim2 <minskim2@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/07/01 12:14:02 by minskim2          #+#    #+#             */
-/*   Updated: 2022/07/08 10:51:46 by minskim2         ###   ########.fr       */
+/*   Created: 2022/07/08 10:54:04 by minskim2          #+#    #+#             */
+/*   Updated: 2022/07/08 12:01:50 by minskim2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
-#include <utility>
 #include <algorithm>
 using namespace std;
-
-bool compare(pair<int, int>& a, pair<int, int>& b) {
-	if (a.second == b.second)
-		return a.first < b.first;
-	return a.second < b.second;
-}
 
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
-	pair<int, int>arr[100000];
-	int n, time=0, cnt=0;
+	int arr[100000];
+	int n, ret=0;
 	cin >> n;
-	for (int i=0, x, y; i<n; i++)
-		cin >> arr[i].first >> arr[i].second;
-	sort(arr, arr+n, compare);
-	for (int i=0; i<n; i++) {
-		if (arr[i].first < time)
-			continue;
-		time = arr[i].second;
-		cnt++;
-	}
-	cout << cnt;
+	for (int i=0; i<n; i++)
+		cin >> arr[i];
+	sort(arr, arr+n);
+	for (int i=n-1; i>=0; i--)
+		ret = max(ret, arr[i] * (n-i));
+	cout << ret;
 }
