b629b6513f7bcd84a469e367bcd8773e120bd0bd
feedbaek <nicek789@naver.com>
2022-07-29 16:07:08 +0900

    이분탐색인척 하는 누적합문제

--- 백준/바킹독/0x13_이분탐색/3020_개똥벌레.cpp
+++ 백준/바킹독/0x13_이분탐색/3020_개똥벌레.cpp
@@ -6,78 +6,45 @@
 /*   By: minskim2 <minskim2@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/22 21:04:07 by minskim2          #+#    #+#             */
-/*   Updated: 2022/06/23 17:37:27 by minskim2         ###   ########.fr       */
+/*   Updated: 2022/07/29 15:39:17 by minskim2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
-#include <vector>
 using namespace std;
 
-int n, h, min_sum;
-vector<int> v;
-vector<int> ret_sum;
-
-bool check_n(int mid) {
-	if (mid >= min_sum)
-		return true;
-	int cnt = 0;
-	for (int height=1; height<=h; height++) {
-		cnt = 0;
-		if (ret_sum[height - 1] != -1) {
-			if (ret_sum[height - 1] > mid)
-				continue;
-			return true;
-		}
-		for (int i=0; i<n; i++) {
-			if (i % 2 == 0) {
-				if (height <= v[i])
-					cnt++;
-			} else {
-				if (height > h - v[i])
-					cnt++;
-			}
-		}
-		ret_sum[height - 1] = cnt;
-		if (cnt < min_sum)
-			min_sum = cnt;
-		if (cnt <= mid)
-			return true;
-	}
-	return false;
-}
-
-int find_ret() {
-	int cnt = 0;
-	for (int i=0; i<h; i++)
-		if (ret_sum[i] == min_sum)
-			cnt++;
-	return cnt;
-}
+int arr1[500001];	// 석순
+int arr2[500001];	// 종유석
+int hit[500001];	// 높이 별 충돌 수
 
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
+	int n, h, x;
 	cin >> n >> h;
-	int x;
-	v.reserve(n);
-	ret_sum.resize(h, -1);
-	min_sum = n;
-	for (int i=0; i<n; i++) {
+	for (int i=0; i<n; ++i) {
 		cin >> x;
-		v.push_back(x);
-	}
-	int st = -1;
-	int en = n;
-	int mid;
-	while (st < en) {
-		mid = (st+en+1) / 2;
-		if (check_n(mid))
-			en = mid - 1;
+		if (i % 2 == 0)
+			++arr1[x];
 		else
-			st = mid;
+			++arr2[x];
 	}
-	x = find_ret();
-	cout << min_sum << " " << x;
+	for (int i=1; i<=h; ++i) {
+		arr1[i] += arr1[i-1];
+		arr2[i] += arr2[i-1];
+	}
+	int min = n, cnt = 0;
+	for (int i=1; i<=h; ++i) {
+		hit[i] = n - (arr1[i-1] + arr2[h-i]);
+		if (hit[i] <= min) {
+			if (hit[i] == min)
+				++cnt;
+			else {
+				cnt = 1;
+				min = hit[i];
+			}
+		}
+	}
+	cout << min << " " << cnt;
 	return 0;
 }
