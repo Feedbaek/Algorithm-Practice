34ed227e8b8e694ad928d5712436d7324346642e
feedbaek <nicek789@naver.com>
2022-08-01 18:32:19 +0900

    바킹독 bfs

--- 백준/바킹독/0x09_BFS/4179_불!.cpp
+++ 백준/바킹독/0x09_BFS/4179_불!.cpp
@@ -6,7 +6,7 @@
 /*   By: minskim2 <minskim2@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/12 09:15:46 by minskim2          #+#    #+#             */
-/*   Updated: 2022/07/30 15:55:40 by minskim2         ###   ########.fr       */
+/*   Updated: 2022/08/01 18:15:46 by minskim2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ using namespace std;
 
 int r, c;
 string map[1000];
+int fire[1000][1000];
 queue<vector<int>> q;
 queue<vector<int>> f;
 
@@ -24,120 +25,66 @@ int main() {
 	cin.tie(0);
 	cin >> r >> c;
 	for (int i=0; i<r; ++i) {
+		int pos = 0;
 		cin >> map[i];
-		if (map[i].find('J') != string::npos)
-			q.push({i, (int)map[i].find('J'), 0});
-		if (map[i].find('F') != string::npos)
-			f.push({i, (int)map[i].find('F')});
+		for (int j=0; j<c; ++j) {
+			if (map[i][j] == 'F') {
+				f.push({i, j, 0});
+				fire[i][j] = 0;
+			}
+			else if (map[i][j] == 'J')
+				q.push({i, j, 0});
+			fire[i][j] = 100000000;
+		}
 	}
 	vector<int> tmp;
-	while (!q.empty()) {
-		int cnt = f.size();
-		for (int i=0; i<cnt; ++i) {
-			tmp = f.front();
-			f.pop();
-			if (tmp[0] > 0 && map[tmp[0]-1][tmp[1]] != '#' && map[tmp[0]-1][tmp[1]] != 'F') {
-				map[tmp[0]-1][tmp[1]] = 'F';
-				f.push({tmp[0]-1, tmp[1]});
-			}
-			if (tmp[0] < r-1 && map[tmp[0]+1][tmp[1]] != '#' && map[tmp[0]+1][tmp[1]] != 'F') {
-				map[tmp[0]+1][tmp[1]] = 'F';
-				f.push({tmp[0]+1, tmp[1]});
-			}
-			if (tmp[1] > 0 && map[tmp[0]][tmp[1]-1] != '#' && map[tmp[0]][tmp[1]-1] != 'F') {
-				map[tmp[0]][tmp[1]-1] = 'F';
-				f.push({tmp[0], tmp[1]-1});
-			}
-			if (tmp[1] < c-1 && map[tmp[0]][tmp[1]+1] != '#' && map[tmp[0]][tmp[1]+1] != 'F') {
-				map[tmp[0]][tmp[1]+1] = 'F';
-				f.push({tmp[0], tmp[1]+1});
-			}
+	while (!f.empty()) {
+		tmp = f.front();
+		if (tmp[0] > 0 && map[tmp[0]-1][tmp[1]] != '#' && map[tmp[0]-1][tmp[1]] != 'F' && fire[tmp[0]-1][tmp[1]] > tmp[2]+1) {
+			fire[tmp[0]-1][tmp[1]] = tmp[2]+1;
+			f.push({tmp[0]-1, tmp[1], tmp[2]+1});
 		}
-		tmp = q.front();
-		if (tmp[0] == 0 || tmp[0] == r-1 || tmp[1] == 0 || tmp[1] == c-1)
-			break;
-		q.pop();
-		if (tmp[0]+1<r && map[tmp[0]+1][tmp[1]] == '.')
-			q.push({tmp[0]+1, tmp[1], tmp[2]+1});
-		if (tmp[0]-1>=0 && map[tmp[0]-1][tmp[1]] == '.')
-			q.push({tmp[0]-1, tmp[1], tmp[2]+1});
-		if (tmp[1]+1<c && map[tmp[0]][tmp[1]+1] == '.')
-			q.push({tmp[0], tmp[1]+1, tmp[2]+1});
-		if (tmp[1]-1>=0 && map[tmp[0]][tmp[1]-1] == '.')
-			q.push({tmp[0], tmp[1]-1, tmp[2]+1});
+		if (tmp[0] < r-1 && map[tmp[0]+1][tmp[1]] != '#' && map[tmp[0]+1][tmp[1]] != '#' && fire[tmp[0]+1][tmp[1]] > tmp[2]+1) {
+			fire[tmp[0]+1][tmp[1]] = tmp[2]+1;
+			f.push({tmp[0]+1, tmp[1], tmp[2]+1});
+		}
+		if (tmp[1] > 0 && map[tmp[0]][tmp[1]-1] != '#' && map[tmp[0]][tmp[1]-1] != '#' && fire[tmp[0]][tmp[1]-1] > tmp[2]+1) {
+			fire[tmp[0]][tmp[1]-1] = tmp[2]+1;
+			f.push({tmp[0], tmp[1]-1, tmp[2]+1});
+		}
+		if (tmp[1] < c-1 && map[tmp[0]][tmp[1]+1] != '#' && map[tmp[0]][tmp[1]+1] != '#' && fire[tmp[0]][tmp[1]+1] > tmp[2]+1) {
+			fire[tmp[0]][tmp[1]+1] = tmp[2]+1;
+			f.push({tmp[0], tmp[1]+1, tmp[2]+1});
+		}
+		f.pop();
 	}
-	cout << q.front()[2] + 1;
+	bool flag = false;
+	while (!q.empty()) {
+		tmp = q.front();
+		if (tmp[0] == 0 || tmp[0] == r-1 || tmp[1] == 0 || tmp[1] == c-1) {
+			flag = true;
+			break;
+		}
+		if (tmp[0]+1<r && map[tmp[0]+1][tmp[1]] == '.' && tmp[2]+1 < fire[tmp[0]+1][tmp[1]]) {
+			fire[tmp[0]+1][tmp[1]] = tmp[2]+1;
+			q.push({tmp[0]+1, tmp[1], tmp[2]+1});
+		}
+		if (tmp[0]-1>=0 && map[tmp[0]-1][tmp[1]] == '.' && tmp[2]+1 < fire[tmp[0]-1][tmp[1]]) {
+			fire[tmp[0]-1][tmp[1]] = tmp[2]+1;
+			q.push({tmp[0]-1, tmp[1], tmp[2]+1});
+		}
+		if (tmp[1]+1<c && map[tmp[0]][tmp[1]+1] == '.' && tmp[2]+1 < fire[tmp[0]][tmp[1]+1]) {
+			q.push({tmp[0], tmp[1]+1, tmp[2]+1});
+			fire[tmp[0]][tmp[1]+1] = tmp[2]+1;
+		}
+		if (tmp[1]-1>=0 && map[tmp[0]][tmp[1]-1] == '.' && tmp[2]+1 < fire[tmp[0]][tmp[1]-1]) {
+			q.push({tmp[0], tmp[1]-1, tmp[2]+1});
+			fire[tmp[0]][tmp[1]-1] = tmp[2]+1;
+		}
+		q.pop();
+	}
+	if (flag)
+		cout << q.front()[2] + 1;
+	else
+		cout << "IMPOSSIBLE";
 }
-
-//queue<pair<int, int>> Q;
-//queue<pair<int, int>> Q1;
-//int	f_vis[1001][1001];
-//int	vis[1001][1001];
-//int	map[1001][1001];
-//int dx[4] = {1, -1, 0, 0};
-//int dy[4] = {0, 0, -1, 1};
-//int cnt_min;
-
-//void	fire_bfs(int n, int m) {
-//	while (!Q.empty()) {
-//		pair<int, int> cur = Q.front();
-//		Q.pop();
-//		for (int dir=0; dir<4; dir++) {
-//			int nx = cur.first + dx[dir];
-//			int ny = cur.second + dy[dir];
-//			if (nx < 0 || nx >= n || ny < 0 || ny >= m)
-//				continue;
-//			if (f_vis[nx][ny] != 0 || map[nx][ny] != '.')
-//				continue;
-//			f_vis[nx][ny] = f_vis[cur.first][cur.second] + 1;
-//			Q.push({nx,ny});
-//		}
-//	}
-//}
-
-//void	jihoon_bfs(int n, int m) {
-//	while (!Q1.empty()) {
-//		pair<int, int> cur = Q1.front();
-//		Q1.pop();
-//		if (cur.first == n-1 || cur.second == m-1) {
-//			if (vis[cur.first][cur.second] < cnt_min)
-//				cnt_min = vis[cur.first][cur.second];
-//			return ;
-//		}
-//		for (int dir=0; dir<4; dir++) {
-//			int nx = cur.first + dx[dir];
-//			int ny = cur.second + dy[dir];
-//			if (nx < 0 || nx >= n || ny < 0 || ny >= m)
-//				continue;
-//			if (vis[nx][ny] != 0 || map[nx][ny] != '.' || vis[cur.first][cur.second] + 1 >= f_vis[nx][ny])
-//				continue;
-//			vis[nx][ny] = vis[cur.first][cur.second] + 1;
-//			Q1.push({nx,ny});
-//		}
-//	}
-//}
-
-//int	main(void) {
-//	ios::sync_with_stdio(0);
-//	cin.tie(0);
-//	int	n, m;
-//	string input;
-//	cin >> n >> m;
-//	for (int i=0; i<n; i++) {
-//		cin >> input;
-//		for (int j=0; j<m; j++) {
-//			map[i][j] = input[j];
-//			if (input[j] == 'F')
-//				Q.push({i, j});
-//			if (input[j] == 'J')
-//				Q1.push({i, j});
-//		}
-//	}
-//	fire_bfs(n, m);
-//	cnt_min = n * m + 10;
-//	jihoon_bfs(n, m);
-//	if (cnt_min == n * m + 10)
-//		cout << "IMPOSSIBLE";
-//	else
-//		cout << cnt_min + 1;
-//}
