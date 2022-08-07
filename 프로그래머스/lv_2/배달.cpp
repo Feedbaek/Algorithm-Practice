defe9872d9ebc0cc0301645aae51ed82af5e5d87
feedbaek <nicek789@naver.com>
2022-08-07 12:52:35 +0900

    8일차(2문제): 예상 대진표, 배달

--- 프로그래머스/lv_2/배달.cpp
+++ 프로그래머스/lv_2/배달.cpp
@@ -6,7 +6,7 @@
 /*   By: minskim2 <minskim2@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/07 10:55:00 by minskim2          #+#    #+#             */
-/*   Updated: 2022/08/07 12:32:27 by minskim2         ###   ########.fr       */
+/*   Updated: 2022/08/07 12:51:15 by minskim2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,14 @@
 #include <deque>
 #define INIT_VALUE 1000000
 using namespace std;
+int visit_cost[51];
 
 int solution(int n, vector<vector<int>> road, int k) {
 	int answer = 1;  // 1번 마을은 무조건 가능해서 1
-	int visit_cost[51];
 	for (int i=2; i<=n; ++i)  // 다른 마을까지의 방문비용 초기 설정
 		visit_cost[i] = INIT_VALUE;
 	deque<int> q;  // 현재 위치를 저장하는 큐
-	for (int i=0; i<road.size(); ++i) { // 도로 검사
-		if (road[i][0] == 1 && road[i][2] <= k && visit_cost[road[i][1]] > road[i][2]) {
-			q.push_back(road[i][1]);
-			visit_cost[road[i][1]] = road[i][2];
-		}  // 1번 마을에서 갈 수있는 도로의 경우 기존의 방문 비용과 비교해서 재설정
-		else if (road[i][1] == 1 && road[i][2] <= k && visit_cost[road[i][0]] > road[i][2]) {
-			q.push_back(road[i][0]);
-			visit_cost[road[i][0]] = road[i][2];
-		}  // 위와 같은데 1번 마을이 road[1]인 경우
-	}
+	q.push_back(1);  // 초기 위치 1 설정
 	int currnet_vil; // 현재 마을위치 저장 변수
 	while (!q.empty()) {
 		currnet_vil = q.front();
