325151847e5746dc95a649d4968d8736821d1307
feedbaek <nicek789@naver.com>
2023-01-16 16:21:19 +0900

    프로그래머스 26일차(1문제): 메뉴 리뉴얼

--- 프로그래머스/lv_2/메뉴 리뉴얼.cpp
+++ 프로그래머스/lv_2/메뉴 리뉴얼.cpp
@@ -1,49 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   메뉴 리뉴얼.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: minskim2 <minskim2@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/16 16:16:42 by minskim2          #+#    #+#             */
+/*   Updated: 2023/01/16 16:16:55 by minskim2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <map>
 
 using namespace std;
 
+int combine[10];
+map<string, int> board_menu;
+int max_cnt[11];
+
+string make_menu(string order) {
+	string menu = "";
+	for (int i=0; i<order.size(); ++i) {
+		if (combine[i] == 1)
+			menu.push_back(order[i]);
+	}
+	sort(menu.begin(), menu.end());
+	return menu;
+}
+
 vector<string> solution(vector<string> orders, vector<int> course) {
 	vector<string> answer;
+	// 크기 == course[i] 메뉴
 	for (int i=0; i<course.size(); ++i) {
-		int uni[26] = {};
-		for (int j=0; j<course[i]; ++j) {
-			uni[j] = 1;
+		// 주문들 순환
+		for (int j=0; j<orders.size(); ++j) {
+			if (orders[j].size() < course[i])
+				continue;
+			// combine 초기화
+			for (int k=0; k<orders[j].size(); ++k) {
+				if (k < course[i])
+					combine[k] = 1;
+				else
+					combine[k] = 0;
+			}
+			// 조합 찾기
+			do {
+				string menu = make_menu(orders[j]);
+				if (!board_menu.insert(make_pair(menu, 1)).second) {
+					++board_menu[menu];
+					if (board_menu[menu] > max_cnt[course[i]])
+						max_cnt[course[i]] = board_menu[menu];
+				}
+			} while (prev_permutation(combine, combine + orders[j].size()));
 		}
-		int _max = 0;
-		do {
-			int cnt = 0;
-			for (int k=0; k<orders.size(); ++k) {
-				int com[26] = {};
-				for (int l=0; l<orders[k].size(); ++l) {
-					com[orders[k][l] - 'A'] = 1;
-				}
-				bool flag = true;
-				for (int l=0; l<26; ++l) {
-					if (uni[l] == 1 && com[l] == 0) {
-						flag = false;
-						break;
-					}
-				}
-				if (flag)
-					++cnt;
-			}
-			if (cnt >= _max && cnt > 1) {
-				string tmp = "";
-				for (int l=0; l<26; ++l) {
-					if (uni[l] == 1)
-						tmp.push_back('A' + l);
-				}
-				if (_max != 0 && cnt > _max) {
-					while (answer.back().size() == tmp.size())
-						answer.pop_back();
-				}
-				answer.push_back(tmp);
-				_max = cnt;
-			}
-		} while (prev_permutation(uni, uni + 26));
 	}
-	sort(answer.begin(), answer.end());
+	for (auto p : board_menu) {
+		if (max_cnt[p.first.size()] == p.second)
+			answer.push_back(p.first);
+	}
 	return answer;
 }
