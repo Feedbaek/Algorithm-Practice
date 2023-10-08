5bca33dc801d42e0c81d3d0feb7fa0a1652a32a8
Feedbaek <nicek789@naver.com>
2023-10-08 22:25:57 +0900

    23.10.08: 시소 짝꿍

--- 프로그래머스/lv_2/시소 짝꿍.cpp
+++ 프로그래머스/lv_2/시소 짝꿍.cpp
@@ -1,48 +1,21 @@
 #include <string>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
 long long solution(vector<int> weights) {
 	long long answer = 0;
+	int m[4001] = {};
+	int w[1001] = {};
 
-	sort(weights.begin(), weights.end());
-
-	for (int i = 0; i < weights.size(); i++) {
-		int st = i;
-		int en = weights.size() - 1;
-		int mid = (st + en + 1) / 2;
-
-		while (st <= en) {
-			mid = (st + en) / 2;
-			if (weights[mid] < weights[i] * 2) {
-				st = mid + 1;
-			}
-			else {
-				en = mid - 1;
-			}
+	for (int i=0; i<weights.size(); i++) {
+		for (int j=2; j<=4; ++j) {
+			if (m[weights[i] * j] > 0)
+				answer += m[weights[i] * j];
+			m[weights[i] * j]++;
 		}
-
-		while (st <= en) {
-			mid = (st + en) / 2;
-			if (weights[mid] < weights[i] * 2) {
-				st = mid + 1;
-			}
-			else {
-				en = mid - 1;
-			}
-		}
-
-		while (st <= en) {
-			mid = (st + en) / 2;
-			if (weights[mid] < weights[i] * 2) {
-				st = mid + 1;
-			}
-			else {
-				en = mid - 1;
-			}
-		}
+		answer -= w[weights[i]] * 2;
+		w[weights[i]]++;
 	}
 	return answer;
 }
