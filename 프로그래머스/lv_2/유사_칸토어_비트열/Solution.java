dc936ab36effade666f9dbc86654c4570d7e7409
Feedbaek <nicek789@naver.com>
2024-01-16 23:47:38 +0900

    24.01.16: 유사 칸토어 비트열

--- 프로그래머스/lv_2/유사_칸토어_비트열/Solution.java
+++ 프로그래머스/lv_2/유사_칸토어_비트열/Solution.java
@@ -2,27 +2,25 @@ package 유사_칸토어_비트열;
 
 public class Solution {
 	public long rec(int n, long pos, long remainder, long[] arr) {
+		if (pos == 0) {
+			return 0;
+		}
 		if (n == 0) {
 			return 1;
 		}
 		long sum = 0;
 		long r = remainder / 5;
 		if (pos <= r * 1) {  // 1
-			System.out.println("select 1, pos: " + pos + ", remainder: " + remainder + ", arr[n-1]: " + arr[n-1]);
 			sum += rec(n-1, pos, r, arr);
 		} else if (pos <= r * 2) {  // 1
-			System.out.println("select 2, pos: " + pos + ", remainder: " + remainder + ", arr[n-1]: " + arr[n-1]);
 			sum = arr[n-1] * 1;
 			sum += rec(n-1, pos - r * 1, r, arr);
 		} else if (pos <= r * 3) {  // 0
-			System.out.println("select 3, pos: " + pos + ", remainder: " + remainder + ", arr[n-1]: " + arr[n-1]);
 			sum = arr[n-1] * 2;
 		} else if (pos <= r * 4) {  // 1
-			System.out.println("select 4, pos: " + pos + ", remainder: " + remainder + ", arr[n-1]: " + arr[n-1]);
 			sum = arr[n-1] * 2;
 			sum += rec(n-1, pos - r * 3, r, arr);
 		} else {  // 1
-			System.out.println("select 5, pos: " + pos + ", remainder: " + remainder + ", arr[n-1]: " + arr[n-1]);
 			sum = arr[n-1] * 3;
 			sum += rec(n-1, pos - r * 4, r, arr);
 		}
@@ -38,17 +36,6 @@ public class Solution {
 			arr[i] = arr[i-1] * 4;
 			s *= 5;
 		}
-
-		//answer = (int) (rec(n, r, s, arr) - rec(n, l-1, s, arr));
-		long resR = rec(n, r, s, arr);
-		long resL = rec(n, l-1, s, arr);
-		System.out.println("resR: " + resR + ", resL: " + resL);
-
-		return (int) (resR - resL);
-	}
-
-	public static void main(String[] args) {
-		Solution sol = new Solution();
-		System.out.println(sol.solution(2, 4, 17));
+		return (int) (rec(n, r, s, arr) - rec(n, l-1, s, arr));
 	}
 }
