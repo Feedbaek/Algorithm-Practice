7c13933dd7849684b8ee57c0a883599874b40fc3
Feedbaek <nicek789@naver.com>
2024-02-11 17:40:20 +0900

    24.02.11: 네트워크

--- 프로그래머스/lv_3/네트워크/Solution.java
+++ 프로그래머스/lv_3/네트워크/Solution.java
@@ -1,6 +1,27 @@
+package 네트워크;
+
 class Solution {
+	private void dfs(int start, int[][] computers, boolean[] visited) {
+		visited[start] = true;
+
+		for (int i = 0; i < computers.length; i++) {
+			if (computers[start][i] == 1 && !visited[i]) {
+				dfs(i, computers, visited);
+			}
+		}
+	}
+
 	public int solution(int n, int[][] computers) {
 		int answer = 0;
+		boolean[] visited = new boolean[n];
+
+		for (int i = 0; i < n; i++) {
+			if (!visited[i]) {
+				dfs(i, computers, visited);
+				answer++;
+			}
+		}
+
 		return answer;
 	}
 }
