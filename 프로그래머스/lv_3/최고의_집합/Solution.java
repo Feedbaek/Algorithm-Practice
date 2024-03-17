e03e7998f2cccf377fdad3956309fb3a5513a581
feedbaek <nicek789@naver.com>
2024-03-17 18:18:28 +0900

    24.03.17: 최고의 집합

--- 프로그래머스/lv_3/최고의_집합/Solution.java
+++ 프로그래머스/lv_3/최고의_집합/Solution.java
@@ -2,7 +2,22 @@ package 최고의_집합;
 
 public class Solution {
 	public int[] solution(int n, int s) {
-		int[] answer = {};
+		int avg = s / n;
+		int[] answer = new int[n];
+		for (int i=0; i<n; ++i) {
+			if (avg == 0) {
+				return new int[]{-1};
+			} else {
+				s -= avg;
+				answer[i] = avg;
+			}
+		}
+		for (int j=n-1; j>=0; --j) {
+			if (s == 0) break;
+			answer[j] += 1;
+			--s;
+		}
+
 		return answer;
 	}
 }
