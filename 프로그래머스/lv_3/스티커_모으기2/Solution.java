622299a2f243b0b8a54ba529bdc258521aa13d3a
Feedbaek <nicek789@naver.com>
2024-03-26 23:09:53 +0900

    24.03.26: 스티커 모으기(2)

---
+++ 프로그래머스/lv_3/스티커_모으기2/Solution.java
@@ -0,0 +1,12 @@
+package 스티커_모으기2;
+
+public class Solution {
+	public int solution(int sticker[]) {
+		int answer = 0;
+
+		// [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+		System.out.println("Hello Java");
+
+		return answer;
+	}
+}
