fcc8b493ff834ce3007efa8802f19af780566775
Feedbaek <nicek789@naver.com>
2024-04-05 23:46:51 +0900

    24.04.05: 가장 먼 노드 진행중

--- 프로그래머스/lv_3/가장_먼_노드/Solution.java
+++ 프로그래머스/lv_3/가장_먼_노드/Solution.java
@@ -1,8 +1,21 @@
 package 가장_먼_노드;
 
+import java.util.*;
+
 public class Solution {
+	List<Integer>[] dir = new ArrayList[20001];
 	public int solution(int n, int[][] edge) {
 		int answer = 0;
+
+		for (int i=0; i<edge.length; ++i) {
+			List<Integer> arr1 = new ArrayList();
+			List<Integer> arr2 = new ArrayList();
+			arr1.add(edge[i][0]);
+			arr2.add(edge[i][1]);
+
+			dir[edge[i][1]] = arr1;
+			dir[edge[i][0]] = arr2;
+		}
 		return answer;
 	}
 }
