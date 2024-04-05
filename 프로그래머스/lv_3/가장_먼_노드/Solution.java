164316ec876648cf5cadae22a16b1e3c13c5d0e3
Feedbaek <nicek789@naver.com>
2024-04-06 00:33:27 +0900

    24.04.05: 가장 먼 노드 진행중

--- 프로그래머스/lv_3/가장_먼_노드/Solution.java
+++ 프로그래머스/lv_3/가장_먼_노드/Solution.java
@@ -3,19 +3,48 @@ package 가장_먼_노드;
 import java.util.*;
 
 public class Solution {
-	List<Integer>[] dir = new ArrayList[20001];
+	boolean[] visited = new boolean[20001];
+	List<Integer>[] direction = new ArrayList[20001];
+	int longDistance = 0;
+	int answer = 0;
+
+
+	void dfs(int node, int distance) {
+		System.out.println("here: " + node);
+		visited[node] = true;
+
+		for (int i=0; i<direction[node].size(); ++i) {
+			int nextNode = direction[node].get(i);
+			if (!visited[nextNode]) {
+				dfs(nextNode, distance + 1);
+			}
+		}
+
+		if (distance > longDistance) {
+			longDistance = distance;
+			System.out.println("longD: " + longDistance);
+			System.out.println("d: " + distance);
+			System.out.println("node: " + node);
+			answer = 1;
+		} else if (distance == longDistance) {
+			System.out.print("++answer");
+			++answer;
+		}
+	}
+
+
 	public int solution(int n, int[][] edge) {
-		int answer = 0;
+		for (int i=1; i<=n; ++i) {
+			direction[i] = new ArrayList();
+		}
 
 		for (int i=0; i<edge.length; ++i) {
-			List<Integer> arr1 = new ArrayList();
-			List<Integer> arr2 = new ArrayList();
-			arr1.add(edge[i][0]);
-			arr2.add(edge[i][1]);
-
-			dir[edge[i][1]] = arr1;
-			dir[edge[i][0]] = arr2;
+			direction[edge[i][1]].add(edge[i][0]);
+			direction[edge[i][0]].add(edge[i][1]);
 		}
+
+		dfs(1, 0);
+
 		return answer;
 	}
 }
