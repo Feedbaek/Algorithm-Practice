fe29c3533b779f41ca9564ff14184ab7f9de4022
Feedbaek <nicek789@naver.com>
2024-04-08 16:14:23 +0900

    24.04.08: 가장 먼 노드

--- 프로그래머스/lv_3/가장_먼_노드/Solution.java
+++ 프로그래머스/lv_3/가장_먼_노드/Solution.java
@@ -3,37 +3,13 @@ package 가장_먼_노드;
 import java.util.*;
 
 public class Solution {
-	boolean[] visited = new boolean[20001];
-	List<Integer>[] direction = new ArrayList[20001];
-	int longDistance = 0;
-	int answer = 0;
-
-
-	void dfs(int node, int distance) {
-		System.out.println("here: " + node);
-		visited[node] = true;
-
-		for (int i=0; i<direction[node].size(); ++i) {
-			int nextNode = direction[node].get(i);
-			if (!visited[nextNode]) {
-				dfs(nextNode, distance + 1);
-			}
-		}
-
-		if (distance > longDistance) {
-			longDistance = distance;
-			System.out.println("longD: " + longDistance);
-			System.out.println("d: " + distance);
-			System.out.println("node: " + node);
-			answer = 1;
-		} else if (distance == longDistance) {
-			System.out.print("++answer");
-			++answer;
-		}
-	}
-
-
 	public int solution(int n, int[][] edge) {
+		boolean[] visited = new boolean[20001];
+		List<Integer>[] direction = new ArrayList[20001];
+		Queue<Integer[]> queue = new LinkedList<>();
+		int longDistance = 0;
+		int answer = 0;
+
 		for (int i=1; i<=n; ++i) {
 			direction[i] = new ArrayList();
 		}
@@ -43,8 +19,32 @@ public class Solution {
 			direction[edge[i][0]].add(edge[i][1]);
 		}
 
-		// dfs가 아닌 dfs로 해결해야함
-		dfs(1, 0);
+		queue.add(new Integer[] {1, 0});
+
+		while (!queue.isEmpty()) {
+			Integer[] poll = queue.poll();
+			int node = poll[0];
+			int distance = poll[1];
+
+			if (visited[node]) {
+				continue;
+			}
+			visited[node] = true;
+
+			for (int i=0; i<direction[node].size(); ++i) {
+				int nextNode = direction[node].get(i);
+				if (!visited[nextNode]) {
+					queue.add(new Integer[] {nextNode, distance + 1});
+				}
+			}
+
+			if (distance > longDistance) {
+				longDistance = distance;
+				answer = 1;
+			} else if (distance == longDistance) {
+				++answer;
+			}
+		}
 
 		return answer;
 	}
