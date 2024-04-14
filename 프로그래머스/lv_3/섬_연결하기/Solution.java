9531d8b3d5709f6542562d51359a87fbb07260f0
Feedbaek <nicek789@naver.com>
2024-04-14 22:23:49 +0900

    24.04.14: 섬 연결하기

--- 프로그래머스/lv_3/섬_연결하기/Solution.java
+++ 프로그래머스/lv_3/섬_연결하기/Solution.java
@@ -3,63 +3,50 @@ package 섬_연결하기;
 import java.util.*;
 
 public class Solution {
-	LinkedList<Integer> path = new LinkedList<>();  // 지나온 경로를 저장하는 리스트
-	int[][] direction = new int[101][101];
-	int[] visited = new int[101];
-	int answer = 0;
+	public int solution(int n, int[][] costs) {
+		Map<Integer, Map<Integer, Integer>> direction = new HashMap<>();
+		Map<Integer, Boolean> visited = new HashMap<>();
+		PriorityQueue<Integer[]> heap = new PriorityQueue<>((a, b) -> {
+			return a[2] - b[2];
+		});
+		int answer = 0;
+		int edgeCount = 0;
 
-	void dfs(int prev, int island) {
-		visited[island] = 1;
-		path.addFirst(island);
-		System.out.println("방문 " + prev + " >> " + island);
-		System.out.println("answer = " + answer);
+		// 사이클을 찾아서 제거하는 문제
+		for (int i=0; i<costs.length; ++i) {
+			if (direction.get(costs[i][0]) == null) {
+				direction.put(costs[i][0], new HashMap<>());
+			}
+			if (direction.get(costs[i][1]) == null) {
+				direction.put(costs[i][1], new HashMap<>());
+			}
+			direction.get(costs[i][0]).put(costs[i][1], costs[i][2]);
+			direction.get(costs[i][1]).put(costs[i][0], costs[i][2]);
+		}
 
-		for (int target=0; target<101; ++target) {
-			if (direction[island][target] == -1 || target == prev || visited[target] == 2) {
+		visited.put(costs[0][0], true);
+		for (Integer i : direction.get(costs[0][0]).keySet()) {
+			heap.add(new Integer[] {costs[0][0], i, direction.get(costs[0][0]).get(i)});
+		}
+		// 시작 노드 추가
+		while (edgeCount != n - 1) {
+			Integer[] p = heap.poll();
+
+			if (visited.get(p[1]) != null) {  // 사이클
 				continue;
 			}
 
-			answer += direction[island][target];
-			if (visited[target] == 1) {  // 사이클 발견
-				System.out.println("중복발견: " + island + "-" + target);
-				Iterator<Integer> iter = path.iterator();
-				int before = target;
-				int maxCost = 0;
-				while (iter.hasNext()) {
-					Integer next = iter.next();
-					System.out.println("next: " + next);
-					if (direction[before][next] > maxCost) {
-						maxCost = direction[before][next];
-					}
-					if (next == target) {  // 사이클의 시작지점
-						answer -= maxCost;
-						System.out.println("제거: " + target + "=>" + maxCost + " answer: " + answer);
-						break;
-					}
-					before = next;
+			visited.put(p[1], true);
+			answer += p[2];
+			edgeCount++;
+
+			for (Integer i : direction.get(p[1]).keySet()) {
+				if (visited.get(i) == null) {
+					heap.add(new Integer[] {p[1], i, direction.get(p[1]).get(i)});
 				}
-			} else if (visited[target] == 0) {  // 처음 방문
-				dfs(island, target);
-			}
-		}
-		visited[island] = 2;  // 탐색 끝
-		path.removeFirst();
-	}
-
-	public int solution(int n, int[][] costs) {
-		// 사이클을 찾아서 제거하는 문제
-		for (int i=0; i<101; ++i) {
-			for (int j=0; j<101; ++j) {
-				direction[i][j] = -1;
 			}
 		}
 
-		for (int i=0; i<costs.length; ++i) {
-			direction[costs[i][0]][costs[i][1]] = costs[i][2];
-			direction[costs[i][1]][costs[i][0]] = costs[i][2];
-		}
-
-		dfs(-1, costs[0][0]);
 		return answer;
 	}
 
@@ -74,6 +61,8 @@ public class Solution {
 		//5 [[0, 1, 1], [2, 3, 1], [3, 4, 2], [1, 2, 2], [0, 4, 100]] 6
 		//5 [[0, 1, 1], [0, 4, 5], [2, 4, 1], [2, 3, 1], [3, 4, 1]] 8
 		//5 [[0, 1, 1], [0, 2, 2], [0, 3, 3], [0, 4, 4], [1, 3, 1]] 8
+		// 4 [[0,1,1],[2,3,1],[1,2,4]] 6
+		// 3 [[0,1,1],[1,2,1],[2,0,10]] 2
 
 		System.out.println(s.solution(5, new int[][]{{0, 1, 5}, {1, 2, 3}, {2, 3, 3}, {3, 1, 2}, {3, 0, 4}, {2, 4, 6}, {4, 0, 7}}) + " == " + 15);
 		System.out.println(s.solution(5, new int[][]{{0, 1, 1}, {3, 4, 1}, {1, 2, 2}, {2, 3, 4}}) + " == " + 8);
@@ -83,5 +72,7 @@ public class Solution {
 		System.out.println(s.solution(5, new int[][]{{0, 1, 1}, {2, 3, 1}, {3, 4, 2}, {1, 2, 2}, {0, 4, 100}}) + " == " + 6);
 		System.out.println(s.solution(5, new int[][]{{0, 1, 1}, {0, 4, 5}, {2, 4, 1}, {2, 3, 1}, {3, 4, 1}}) + " == " + 8);
 		System.out.println(s.solution(5, new int[][]{{0, 1, 1}, {0, 2, 2}, {0, 3, 3}, {0, 4, 4}, {1, 3, 1}}) + " == " + 8);
+		System.out.println(s.solution(4, new int[][]{{0, 1, 1}, {2, 3, 1}, {1, 2, 4}}) + " == " + 6);
+		System.out.println(s.solution(3, new int[][]{{0, 1, 1}, {1, 2, 1}, {2, 0, 10}}) + " == " + 2);
 	}
 }
