18f156ea4df70078047b4a23b8126c14c6a3be3a
Feedbaek <nicek789@naver.com>
2024-05-02 01:04:02 +0900

    24.05.01: 입국심사 진행중

---
+++ 프로그래머스/lv_3/입국심사/Solution.java
@@ -0,0 +1,12 @@
+package 입국심사;
+
+public class Solution {
+	public long solution(int n, int[] times) {
+		// 이분탐색으로 심사 시간을 정함
+		// 심사 시간을 넘지 않게 앞에서 부터 모든 심사관들에게 사람들을 할당함
+		// 성공한 경우 시간을 낮추고, 실패한 경우 시간을 높임
+
+		long answer = 0;
+		return answer;
+	}
+}
